11d5886e1384b00fc77dccbededb38f22b913327
saumittras <saumittra.eee@gmail.com>
2024-10-24 00:12:54 +0600

    daisyUI added

--- src/assets/Component/Allproducts/Allproducts.jsx
+++ src/assets/Component/Allproducts/Allproducts.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Allproducts.css";
+import SingleProduct from "../SingleProduct/SingleProduct";
 
 const Allproducts = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("./fakeData.json")
+      .then((res) => res.json())
+      .then((data) => setProducts(data.products));
+  }, []);
   return (
     <div>
       <h1 className="text-2xl font-bold">AllProducts.jsx</h1>
+      <SingleProduct></SingleProduct>
+      {products.map((p) => (
+        <SingleProduct product={p} key={p.id}></SingleProduct>
+      ))}
     </div>
   );
 };
---
+++ src/assets/Component/SingleProduct/SingleProduct.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./SingleProduct.css";
+const SingleProduct = ({ product }) => {
+  console.log(product);
+  return (
+    <div>
+      <h1>SingleProduct.jsx</h1>
+    </div>
+  );
+};
+
+export default SingleProduct;
